8e71ebb3d06ca79305e1792de2735e213668fc0b
ShivYadav12 <93070295+ShivYadav12@users.noreply.github.com>
2025-07-11 20:57:05 +0530

    Update ecr.tf

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,3 +1,4 @@
+## ecr.tf is just a Terraform file (with .tf extension) that defines AWS Elastic Container Registry (ECR) resources.
 resource "aws_ecr_repository" "medusa" {
   name = "${var.project_name}-repo"
 }
