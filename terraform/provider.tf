1cdeb4c5d1a9d70dab5d7e5b7ca6d602c4d8bd03
ShivYadav12 <93070295+ShivYadav12@users.noreply.github.com>
2025-07-11 21:02:36 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,4 @@
+### provider.tf is a Terraform configuration file where you define the cloud provider (like AWS, Azure, GCP) your infrastructure will be created on.
 provider "aws" {
   region = var.aws_region
 }
